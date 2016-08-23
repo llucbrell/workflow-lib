851f9a79c94d00843d54c21810ed069b234d7ecd
Lucas <lucas@gummo>
2016-08-23 20:30:20 +0200

    added jshint, jscs, jsdocs, and file-generator

--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,22 @@
 module.exports = function (grunt) {
+    
+    
+    // ---------------------- CONSTANT sources  ------------------------------ //
+    
+    var initial_directories = ['.tmp', '.tmp/doc', 'test', 'js', 'built'],
+        javascript_to_docs = ['js/**/*.js'],
+        javascript_files = ['js/**/*.js', '*.js'],
+        destination_docs = ".tmp/doc",
+        build_docs = "built/doc",
+        
+    // ---------------------- CONSTANT options  ------------------------------ //
 
-    grunt.initConfig({
+        jshint_rules = {laxbreak: true},
         
-        // to use properties of the project guided by package.json file
-        pkg: grunt.file.readJSON('package.json'),
         
-        // building project principal structure
-        mkdir: {
-            all: {
-                options: {
-                create: ['.tmp', 'test', 'js','built']
-                }
-            }
-        },
+    // --------------------- TEXT values ------------------------------------- //
         
-        // building project files         
-          "file-creator": {
-            "basic": {
-              "readme.md": function(fs, fd, done) {
-                fs.writeSync(fd, 
-                             "[comment]: <> (example of image -->   ![](https://raw.githubusercontent.com/llucbrell/audrey2/master/audrey.png)   )" 
+        readme_text = "[comment]: <> (example of image -->   ![](https://raw.githubusercontent.com/llucbrell/audrey2/master/audrey.png)   )" 
                              +"\n"
                              +"[comment]: <> (example of Travis build integration -->[![Build Status](https://travis-ci.org/llucbrell/audrey2.svg?branch=master)](https://travis-ci.org/llucbrell/audrey2)    )" 
                              +"\n"
@@ -37,12 +34,9 @@ module.exports = function (grunt) {
                              +"When we build a CLI (Command Line Interface) we can find some troubles. While the software stays small it's easy to mantain and work with it. But as much a it grows... It could be a work for something out of space."
                              +"\n"
                              +"[comment]: <> (example of link -----> [link](https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller)           )"
-                             +"\n"
-                            );
-                done();
-              },
-            "license.md": function(fs, fd, done) {
-                fs.writeSync(fd, "The MIT License"
+                             +"\n",
+        
+        license_text =  "The MIT License"
                                 +"==============="
                                 +"\n"
                                 +"Copyright (c) Lucas_C / llucbrell 2015"
@@ -63,55 +57,152 @@ module.exports = function (grunt) {
                                 +"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER"
                                 +"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,"
                                 +"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN"
-                                +"THE SOFTWARE."
-                                );
+                                +"THE SOFTWARE.",
+        
+        gitignore_text = "#ignore node modules\n"
+                                +"node_modules\n"
+                                +"#ignore grunt temporary files\n"
+                                +".tmp\n",
+        
+        npmignore_text = "#ignore node modules\n" //revisar
+                                +"node_modules\n"
+                                +"#ignore grunt temporary files\n"
+                                +".tmp\n"
+                                +"#ignore git files\n"
+                                +".git\n",
+        
+        indexjs_text = "\n" ,
+        
+        travis_text =  "language: node_js\n" //revisar
+                                +"node_js:\n"
+                                +' - "6"\n'
+                                +' - "5"\n'
+                                +' - "4"\n'
+                                +' - "0.12"\n'
+                                +' - "0.11"\n'
+                                +' - "0.10"\n'
+                                +' - "iojs"\n'
+                                +' - "iojs-v1.0.4"\n'
+                                +'before_install:'
+                                +' - npm install -g jasmine\n'
+                                +' - npm install -g npm\n'
+                                +' - npm install -g grunt-cli\n',
+        
+        check_text = "\n"
+        
+        ; 
+    
+    // ---------------------------------------------------------------  //
+
+    
+    
+    
+    // ---------------------- GRUNT CONFIG TASKS ---------------------  //
+    grunt.initConfig({
+        
+        // to use properties of the project guided by package.json file
+        pkg: grunt.file.readJSON('package.json'),
+        
+        // building project principal structure
+        mkdir: {
+            all: {
+                options: {
+                create: initial_directories
+                }
+            }
+        },
+        
+        // building project files  index.js, travis, readme, license, gitignore, npmignore       
+          "file-creator": {
+            "generate-files": {
+              "readme.md": function(fs, fd, done) {
+                fs.writeSync(fd, readme_text);
+                done();
+              },
+            "license.md": function(fs, fd, done) {
+                fs.writeSync(fd,license_text);
                 done();
               },
                 ".gitignore": function(fs, fd, done) {
-                fs.writeSync(fd, "#ignore node modules\n"
-                                 +"node_modules\n"
-                                 +"#ignore grunt temporary files\n"
-                                 +".tmp\n"
-                                );
-                done();
+                    fs.writeSync(fd, gitignore_text);
+                    done();
               },
                ".npmignore": function(fs, fd, done) {
-                fs.writeSync(fd, "#ignore node modules\n" //revisar
-                                 +"node_modules\n"
-                                 +"#ignore grunt temporary files\n"
-                                 +".tmp\n"
-                                 +"#ignore git files\n"
-                                 +".git\n"
-                                );
-                done();
+                    fs.writeSync(fd, npmignore_text);
+                    done();
+              },
+                "index.js": function(fs, fd, done) {
+                    fs.writeSync(fd, indexjs_text); //revisar      
+                    done();
               },
                 ".travis.yml": function(fs, fd, done) {
-                fs.writeSync(fd, "language: node_js\n" //revisar
-                                 +"node_js:\n"
-                                 +' - "6"\n'
-                                 +' - "5"\n'
-                                 +' - "4"\n'
-                                 +' - "0.12"\n'
-                                 +' - "0.11"\n'
-                                 +' - "0.10"\n'
-                                 +' - "iojs"\n'
-                                 +' - "iojs-v1.0.4"\n'
-                                 +'before_install:'
-                                 +' - npm install -g jasmine\n'
-                                 +' - npm install -g npm\n'
-                                 +' - npm install -g grunt-cli\n'
-                                );
-                done();
+                    fs.writeSync(fd, travis_text);
+                    done();
+              }
+            },
+             // second use.. generate more files when it's demand 
+            "gen":{
+               "checktext.txt": function(fs, fd, done) {
+                    fs.writeSync(fd, check_text);
+                    done();
               }
             }
-          }
+          },
+        
+        // Documentation generation
+        
+        jsdoc:{
+            // for temporary consult
+            "dev-work":{
+                src: javascript_to_docs ,
+                options:{
+                    destination: destination_docs
+                }
+            },
+            // for build process
+            "dev-build":{
+                src: javascript_to_docs ,
+                options:{
+                    destination: build_docs
+                }
+            }
+        },
+        
+        // lint javascript files
+        
+        jshint:{
+            check:{
+                src: javascript_files,
+                options: jshint_rules
+                
+            }
+        },
+        
+        //styling javascript files
+        
+        jscs: {
+            src: javascript_files,
+                options: { //configurar bien
+                    config: ".jscsrc",
+                    //esnext: true, // If you use ES6 http://jscs.info/overview.html#esnext 
+                    fix: true, // Autofix code style violations when possible. 
+                    requireCurlyBraces: [ "if" ]
+            }
+        }
         
     });
     
     // load plugin
     grunt.loadNpmTasks('grunt-mkdir');
     grunt.loadNpmTasks('grunt-file-creator');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
+    
     
     // default tasks
-    grunt.registerTask('create', ['mkdir', 'file-creator']);
+    grunt.registerTask('create', ['mkdir', 'file-creator:generate-files']);
+    grunt.registerTask('work', ['file-creator:gen', 'jsdoc:dev-work', 'jshint:check']);
+    grunt.registerTask('build', ['file-creator:gen', 'jsdoc:dev-build','jshint:check', 'jscs']);
+
 };
