061bb0ec6f57cd223f54defc0096e23bd2cad2f3
Lucas_C <hobbescode@gmail.com>
2016-08-26 19:30:50 +0200

    fixed bug .files and .files.yml

--- .build.sh
+++ .build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp -r ./.tmp/built/* ./
+cp ./.tmp/built/.* ./
+cp ./.tmp/built/.* ./
 echo 'restored dependency and data files'
 cp -r ./.tmp/replacements/* ./
 echo 'restored library'
